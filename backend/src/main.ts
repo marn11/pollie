a4a5fe516f7674543d2bcbd5421a8d7b42c38f1d
marin bench <yourintv.84@gmail.com>
2026-02-19 19:52:41 +0000

    enabling cors and validation pipes for dtos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser = require('cookie-parser');
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: 'http://localhost:3000' });
+  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
